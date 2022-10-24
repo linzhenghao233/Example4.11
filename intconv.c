27e3b3fb470d26ce9fc531118427897d8079b915
林政浩 <1392872316@qq.com>
2022-10-24 21:49:17 +0800

    添加项目文件。

---
+++ intconv.c
@@ -0,0 +1,21 @@
+/* intconv.c -- 一些不匹配的整形转换 */
+#include <stdio.h>
+#define PAGES 336
+#define WORDS 65618
+int main(void) {
+	short num = PAGES;
+	short mnum = -PAGES;
+
+	printf("num as short and unsigned short: %hd %hu\n", num, num);
+	printf("-num as short and unsigned short %hd %hu\n", mnum, mnum);
+	printf("num as int and char: %d %c\n", num, num);
+	printf("WORDS as int, short, and char: %d %hd %c\n", WORDS, WORDS, WORDS);
+
+	return 0;
+}
+
+//运行结果：
+//num as short and unsigned short: 336 336
+//-num as short and unsigned short -336 65200
+//num as int and char: 336 P
+//WORDS as int, short, and char: 65618 82 R
